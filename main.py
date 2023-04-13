3e751a6cc8f4195a7cca965de65dd1d24547ce2d
Kerbolerr <david.galvez.alcantara@estudiantat.upc.edu>
2023-04-13 16:49:37 +0200

    MaxPrice update

--- main.py
+++ main.py
@@ -17,21 +17,37 @@ from sklearn import ensemble
 from sklearn.svm import SVC
 from sklearn import metrics
 
-tabletModel = joblib.load(open("./Model/Tablet.sav", 'rb'))
-featuresScalerTablet = joblib.load(open("./Model/scalers/scalerFeaturesTablet.skl",'rb'))
-targetScalerTablet = joblib.load(open("./Model/scalers/scalerTargetTablet.skl",'rb'))
+tabletModel = joblib.load(open("./Model/Models/MinPrice/Tablet.sav", 'rb'))
+featuresScalerTablet = joblib.load(open("./Model/scalers/MinPrice/scalerFeaturesTablet.skl",'rb'))
+targetScalerTablet = joblib.load(open("./Model/scalers/MinPrice/scalerTargetTablet.skl",'rb'))
 
-smartphoneModel = joblib.load(open("./Model/Smartphone.sav", 'rb'))
-featuresScalerSmartphone = joblib.load(open("./Model/scalers/scalerFeaturesSmartphone.skl",'rb'))
-targetScalerSmartphone = joblib.load(open("./Model/scalers/scalerTargetSmartphone.skl",'rb'))
+smartphoneModel = joblib.load(open("./Model/Models/MinPrice/Smartphone.sav", 'rb'))
+featuresScalerSmartphone = joblib.load(open("./Model/scalers/MinPrice/scalerFeaturesSmartphone.skl",'rb'))
+targetScalerSmartphone = joblib.load(open("./Model/scalers/MinPrice/scalerTargetSmartphone.skl",'rb'))
 
-desktopModel = joblib.load(open("./Model/Desktop.sav", 'rb'))
-featuresScalerDesktop = joblib.load(open("./Model/scalers/scalerFeaturesDesktop.skl",'rb'))
-targetScalerDesktop = joblib.load(open("./Model/scalers/scalerTargetDesktop.skl",'rb'))
+desktopModel = joblib.load(open("./Model/Models/MinPrice/Desktop.sav", 'rb'))
+featuresScalerDesktop = joblib.load(open("./Model/scalers/MinPrice/scalerFeaturesDesktop.skl",'rb'))
+targetScalerDesktop = joblib.load(open("./Model/scalers/MinPrice/scalerTargetDesktop.skl",'rb'))
 
-laptopModel = joblib.load(open("./Model/Laptop.sav", 'rb'))
-featuresScalerLaptop = joblib.load(open("./Model/scalers/scalerFeaturesLaptop.skl",'rb'))
-targetScalerLaptop = joblib.load(open("./Model/scalers/scalerTargetLaptop.skl",'rb'))
+laptopModel = joblib.load(open("./Model/Models/MinPrice/Laptop.sav", 'rb'))
+featuresScalerLaptop = joblib.load(open("./Model/scalers/MinPrice/scalerFeaturesLaptop.skl",'rb'))
+targetScalerLaptop = joblib.load(open("./Model/scalers/MinPrice/scalerTargetLaptop.skl",'rb'))
+
+tabletModelMaxPrice = joblib.load(open("./Model/Models/MaxPrice/Tablet.sav", 'rb'))
+featuresScalerTabletMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerFeaturesTablet.skl",'rb'))
+targetScalerTabletMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerTargetTablet.skl",'rb'))
+
+smartphoneModelMaxPrice = joblib.load(open("./Model/Models/MaxPrice/Smartphone.sav", 'rb'))
+featuresScalerSmartphoneMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerFeaturesSmartphone.skl",'rb'))
+targetScalerSmartphoneMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerTargetSmartphone.skl",'rb'))
+
+desktopModelMaxPrice = joblib.load(open("./Model/Models/MaxPrice/Desktop.sav", 'rb'))
+featuresScalerDesktopMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerFeaturesDesktop.skl",'rb'))
+targetScalerDesktopMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerTargetDesktop.skl",'rb'))
+
+laptopModelMaxPrice = joblib.load(open("./Model/Models/MaxPrice/Laptop.sav", 'rb'))
+featuresScalerLaptopMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerFeaturesLaptop.skl",'rb'))
+targetScalerLaptopMaxPrice = joblib.load(open("./Model/scalers/MaxPrice/scalerTargetLaptop.skl",'rb'))
 
 def prepareForModelTablet(screenSize,storage,ram,resolution,yearOfLaunch,megapixels,model):
     models = ["iPad","iPad Pro","iPad Air","iPad mini","Galaxy Tab"]
@@ -93,19 +109,23 @@ def root():
 
 @app.get("/tablet/")
 def read_root(screenSize,storage,ram,resolution,yearOfLaunch,megapixels,model):
-    return {"price":targetScalerTablet.inverse_transform([tabletModel.predict(featuresScalerTablet.transform([prepareForModelTablet(screenSize,storage,ram,resolution,yearOfLaunch,megapixels,model)]))])[0][0]}
+    return {"MinPrice":targetScalerTablet.inverse_transform([tabletModel.predict(featuresScalerTablet.transform([prepareForModelTablet(screenSize,storage,ram,resolution,yearOfLaunch,megapixels,model)]))])[0][0],
+    "MaxPrice":targetScalerTabletMaxPrice.inverse_transform([tabletModelMaxPrice.predict(featuresScalerTabletMaxPrice.transform([prepareForModelTablet(screenSize,storage,ram,resolution,yearOfLaunch,megapixels,model)]))])[0][0]}
 
 @app.get("/smartphone/")
 def read_root(screenSize,storage,ram,megapixels,resolution,bandwith,yearOfLaunch,model):
-    return {"price":targetScalerSmartphone.inverse_transform([smartphoneModel.predict(featuresScalerSmartphone.transform([prepareForModelSmartphone(screenSize,storage,ram,megapixels,resolution,bandwith,yearOfLaunch,model)]))])[0][0]}
+    return {"MinPrice":targetScalerSmartphone.inverse_transform([smartphoneModel.predict(featuresScalerSmartphone.transform([prepareForModelSmartphone(screenSize,storage,ram,megapixels,resolution,bandwith,yearOfLaunch,model)]))])[0][0],
+    "MaxPrice":targetScalerSmartphoneMaxPrice.inverse_transform([smartphoneModelMaxPrice.predict(featuresScalerSmartphoneMaxPrice.transform([prepareForModelSmartphone(screenSize,storage,ram,megapixels,resolution,bandwith,yearOfLaunch,model)]))])[0][0]}
 
 @app.get("/desktop/")
 def read_root(hddStorage,sddStorage,ram,yearOfLaunch,brand,graphicsModel,cpuModel):
-    return {"price":targetScalerDesktop.inverse_transform([desktopModel.predict(featuresScalerDesktop.transform([prepareForModelDesktop(hddStorage,sddStorage,ram,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0]}
+    return {"MinPrice":targetScalerDesktop.inverse_transform([desktopModel.predict(featuresScalerDesktop.transform([prepareForModelDesktop(hddStorage,sddStorage,ram,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0],
+    "MaxPrice":targetScalerDesktopMaxPrice.inverse_transform([desktopModelMaxPrice.predict(featuresScalerDesktopMaxPrice.transform([prepareForModelDesktop(hddStorage,sddStorage,ram,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0]}
 
 @app.get("/laptop/")
 def read_root(screenSize,hddStorage,ram,sddStorage,resolution,yearOfLaunch,brand,graphicsModel,cpuModel):
-    return {"price":targetScalerLaptop.inverse_transform([laptopModel.predict(featuresScalerLaptop.transform([prepareForModelLaptop(screenSize,hddStorage,ram,sddStorage,resolution,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0]}
+    return {"MinPrice":targetScalerLaptop.inverse_transform([laptopModel.predict(featuresScalerLaptop.transform([prepareForModelLaptop(screenSize,hddStorage,ram,sddStorage,resolution,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0],
+    "MaxPrice":targetScalerLaptopMaxPrice.inverse_transform([laptopModelMaxPrice.predict(featuresScalerLaptopMaxPrice.transform([prepareForModelLaptop(screenSize,hddStorage,ram,sddStorage,resolution,yearOfLaunch,brand,graphicsModel,cpuModel)]))])[0][0]}
 
 if __name__ == "__main__":
     uvicorn.run(
